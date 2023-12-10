0da3c318f18838f5edbc5db739f06bcfb59c4fb8
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-12-10 19:42:47 +0700

    hw-2 на проверку

--- src/index.js
+++ src/index.js
@@ -12,14 +12,17 @@ const port = 3003;
 const hostname ="http://127.0.0.1";
 
 const server = http.createServer((request, response) => {
-  const url = new URL(request.url, hostname);
-  const userName = url.searchParams.get("name");
+  //const url = new URL(request.url, hostname);
+  //const userName = url.searchParams.get('hello');
+  const params = new URLSearchParams()
+  params.append('hello', 'Ivan')
+
 
   if (request.url === '/hello=name') {
     response.statusCode = 200;
     response.statusMessage = "ok";
     response.setHeader("Content-Type", "text/plain");
-    response.write(`Hello ${userName}`);
+    response.write(`Hello, my friend ${params.get("hello")}`);
     response.end();
     return;
   };
